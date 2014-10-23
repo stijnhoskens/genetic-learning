3a7d040340ce4784f62b8a179ec2dec5a4820495
stijnhoskens <stijn.hoskens@student.kuleuven.be>
2014-10-23 14:34:50 +0200

    made it a bit more elegant

--- Masterproef/src/pre/Preprocessing.java
+++ Masterproef/src/pre/Preprocessing.java
@@ -4,7 +4,6 @@ import java.nio.file.Path;
 
 import pre.data.DataSet;
 import pre.data.DataSplitter;
-import pre.data.ExplicitDataSorter;
 import pre.parsers.CoraParser;
 import pre.parsers.DataSetParser;
 import pre.text.Vocabulary;
@@ -15,14 +14,25 @@ public class Preprocessing {
 
 	public static void main(String[] args) throws Exception {
 		// replace this with the desired dataset parser
-		DataSetParser parser = new CoraParser();
+		preprocess(new CoraParser());
+	}
+
+	public static void preprocess(DataSetParser parser) throws Exception {
 		DataSet data = parser.getData();
 		Path output = parser.parse(data.directory());
+		System.out.println("parsing done");
 		DataSplitter.split(output, data, 0.3d);
+		System.out.println("splitting done");
 		Vocabulary.buildAndExport(data);
 		Vocabulary.WithFrequency.buildAndExport(data);
+		System.out.println("building vocabulary done");
 		VocabularyFilter.filter(data);
-		ExplicitDataSorter.sort(data);
+		System.out.println("filtering done");
+		// ExplicitDataSorter.sort(data);
+		// System.out.println("sorting done");
 		WordIndexer.index(data);
+		System.out.println("indexing done");
+		System.out.println("preprocessing done");
+
 	}
 }
